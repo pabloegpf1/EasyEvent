1db98d82197dca10bcbb3f0a8dbae357e4adabb2
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-11-26 17:33:00 +0100

    Fixing draggable

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,44 @@
 $(document).ready(function () {
 
-    // Close task
-    $(".close").click(function () {
-        if (confirm("Are you sure you want to delete this task?")) {
+    // Close event
+    $(".close").on('click', function () {
+        if (confirm("Are you sure you want to delete this event?")) {
             $(this).parent().hide()
         }
     });
 
-    // Like task
-    $(".action").click(function () {
-        if ($(this).find('img').hasClass('like')) {
-            if ($(this).find('img').attr('src') == 'images/like.png') {
-                $(this).find('img').attr('src', 'images/like-active.png');
-            } else {
-                $(this).find('img').attr('src', 'images/like.png');
-            }
+    // Like event
+    $(".like").on('click',function () {
+        if ($(this).attr('src') == 'images/like.png') {
+            $(this).attr('src', 'images/like-active.png');
+        } else {
+            $(this).attr('src', 'images/like.png');
         }
     });
 
-    // Drag a task
-    $('.task').draggable({
+    // Drag a event
+    $('.event').draggable({
         cursor: 'move',
         revert: true,
         revertDuration: 0,
         delay: 200,
-        connectToSortable: ".column",
+        connectToSortable: ".event-list",
         stop: function (event, ui) {
             $(ui.helper).css('width', "");
         }
     });
 
+    // Drop a event inside a column
+    $(".event-list").sortable({
+        items: ".event",
+    }).disableSelection();
 
-    // Drop a task inside a column
-    $(".column").sortable({
-        items: ".task",
+    // Drop a event inside a column
+    $(".event-list").droppable({
+        accept: ".event",
     });
 
-    // Drop a task inside a column
-    $(".column").droppable({
-        accept: ".task",
-    });
-
-    // Drag a task
+    // Drag a category
     $('.column').draggable({
         cursor: 'move',
         delay: 200,
@@ -63,14 +60,14 @@ $(document).ready(function () {
     });
 
     // Show column options 
-    $('.column-options').click(function (event) {
+    $('.column-options').on('click',function (event) {
         $('.column-options-dropdown').hide();
         $(this).parent().find('.column-options-dropdown').css('display', 'flex');
         event.stopPropagation();
     });
 
     // Hide column options when click outside 
-    $(document).click(function () {
+    $(document).on('click',function () {
         $('.column-options-dropdown').hide();
     });
 
@@ -79,6 +76,24 @@ $(document).ready(function () {
         if (confirm("Are you sure you want to delete this column?")) {
             $(this).parent().parent().hide("3000");
         }
+    });
+
+    $(".new-event").on('submit',function (event) {
+        let title = $(this).find('input[name="title"]').val();
+        $(this).before(`
+            <div class="event">
+                <a class="close" href="#"><img src="images/close.png" alt="close"></a>
+                <h3>`
+                +title+
+                `</h3>
+                <ul class="event-description">
+                    <li><img class="like new" src="images/like.png" alt="like"></li>
+                    <li>03/06/19 15:47</li>
+                </ul>
+            </div>
+        `)
+        $(this).find('input[name="title"]').val('') 
+        event.preventDefault();
     });
 
 });
