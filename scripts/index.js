5815ca6705dbe4e34d50bc2eb517b8c20b018fbe
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-12-02 16:21:03 +0100

    Create event and category functions

--- scripts/index.js
+++ scripts/index.js
@@ -7,16 +7,9 @@ $(document).ready(function () {
         event.stopPropagation();
         let title = $(this).find('input[name="title"]').val();
         let date = $(this).find('input[name="date"]').val();
-        $(this).parent().find('.event-list').append(`
-            <div class="event">
-                <a class="close" href="#"><img src="images/close.png" alt="close"></a>
-                <h3>`+ title + `</h3>
-                <ul class="event-description">
-                    <li><img class="like new" src="images/like.png" alt="like"></li>
-                    <li>`+ date + `</li>
-                </ul>
-            </div>
-        `)
+        let categoryTitle = $(this).parent().find('h2').html();
+        console.log(categoryTitle)
+        createEvent(categoryTitle,title,date);
         $(this).find('input[name="title"]').val('')
         $(this).find('input[name="date"]').val('')
         addEventListeners();
@@ -26,35 +19,18 @@ $(document).ready(function () {
         event.preventDefault();
         event.stopPropagation();
         let title = $(this).find('input[name="title"]').val();
-        $(this).before(`
-            <div class="category">
-                <a class="category-options" href="#"><img src="images/menu.png" alt="options"></a>
-                <div class="category-options-dropdown">
-                    <a class="close-category" href="#">Archive list</a>
-                    <a href="#">Clone list</a>
-                    <a href="#">Share list</a>
-                    <a href="#">Settings</a>
-                </div>
-                <h1>`+ title + `</h1>
-                <div class="event-list">
-                </div>
-                <form class="new-event">
-                    <input class="form-control mr-sm-1" name="title" placeholder="Title" aria-label="New Event">
-                    <input class="form-control mr-sm-1" name="date" type="date" aria-label="date">
-                </form>
-            </div>
-        `)
+        createCategory(title);
         $(this).find('input[name="title"]').val('')
         addEventListeners();
     });
 
 });
 
-function addEventListeners(){
+function addEventListeners() {
 
     // Close event
     $(".close").on('click', function () {
-        if (confirm("Are you sure you want to delete this event?")) {
+        if (confirm("Are you sure you want to delete this?")) {
             $(this).parent().hide()
         }
     });
@@ -130,75 +106,115 @@ function addEventListeners(){
         }
     });
 
-    $(".new-event").on('submit',function (event) {
-        let title = $(this).find('input[name="title"]').val();
-        $(this).before(`
-            <div class="event">
-                <a class="close" href="#"><img src="images/close.png" alt="close"></a>
-                <h3>`
-                +title+
-                `</h3>
-                <ul class="event-description">
-                    <li><img class="like new" src="images/like.png" alt="like"></li>
-                    <li>03/06/19 15:47</li>
-                </ul>
-            </div>
-        `)
-        $(this).find('input[name="title"]').val('') 
-        event.preventDefault();
-    });
-
     $(".div-regist a").on("click", function () {
-        $(".alertLog").addClass("show");
+        $(".alertLog").addClass("showjs");
     });
 
-    $(".regist").submit(function(evt){
+    $(".regist").submit(function (evt) {
         var inputsArray = Array.from($(".regist input.field"));
-        
+
         console.log(inputsArray);
         let inputs = $(this).parent().parent().find("input");
-        
-        if (findCookie(inputsArray)>0) {
+
+        if (findCookie(inputsArray) > 0) {
             evt.preventDefault();
-            alert("El email: "+inputsArray[0].value+" ya existe");
-        }else{
+            alert("El email: " + inputsArray[0].value + " ya existe");
+        } else {
             storeCookie(inputsArray);
         }
     });
 
-    $(".login").submit(function (evt){
+    $(".login").submit(function (evt) {
         evt.preventDefault();
         var inputsArray = Array.from($(".login input.field"));
         console.log(inputsArray[1].value);
         let found = findCookie(inputsArray);
         if (found == 2) {
             window.location.replace("list.html");
-        }else{
-            alert("Usuario y/o contraseña erroneos "+found);
+        } else {
+            alert("Usuario y/o contraseña erroneos " + found);
+        }
+    });
+
+    // Reject notification
+    $(".reject").on('click', function () {
+        if (confirm("Are you sure you want to reject?")) {
+            $(this).parent().hide()
+        }
+    });
+
+    // Accept notification
+    $(".join").on('click', function () {
+        if (confirm("Do you want to add this event to your list?")) {
+            $(this).parent().hide();
         }
     });
 
 }
 
-function checkform(elem){
-    if(elem.value == ""){
+function createCategory(title) {
+     $('.new-category').before(`
+            <div class="category">
+                <a class="category-options" href="#"><img src="images/menu.png" alt="options"></a>
+                <div class="category-options-dropdown">
+                    <a class="close-category" href="#">Archive list</a>
+                    <a href="#">Clone list</a>
+                    <a href="#">Share list</a>
+                    <a href="#">Settings</a>
+                </div>
+                <h2>`+ title + `</h2>
+                <div class="event-list">
+                </div>
+                <form class="new-event">
+                    <input class="form-control mr-sm-1" name="title" placeholder="Title" aria-label="New Event">
+                    <input class="form-control mr-sm-1" name="date" type="date" aria-label="date">
+                </form>
+            </div>
+        `) 
+}
+
+function createEvent(categoryTitle,title,date){
+    let categoryPointer = undefined;
+    $(".category").each(function (index) {
+        if ($(this).find('h2').html() == categoryTitle){
+            categoryPointer = $(this);
+        }
+    });
+    if (categoryPointer == undefined){
+        createCategory(categoryTitle);
+        categoryPointer = $('.new-category').prev();
+    }
+    categoryPointer.find('.event-list').append(`
+            <div class="event">
+                <a class="close" href="#"><img src="images/close.png" alt="close"></a>
+                <h3>`+ title + `</h3>
+                <ul class="event-description">
+                    <li><img class="like new" src="images/like.png" alt="like"></li>
+                    <li>`+ date + `</li>
+                </ul>
+            </div>
+        `)
+}
+
+function checkform(elem) {
+    if (elem.value == "") {
     }
 }
 
-function storeCookie(array){
+function storeCookie(array) {
     let storeString = "";
     storeString = array[0].value + " = ";
     array.slice(1).forEach(elem => {
-        storeString += elem.value +",";
+        storeString += elem.value + ",";
     });
     storeString += ";path=/";
     document.cookie = storeString;
 }
 
-function findCookie(array){
-    
+function findCookie(array) {
+
     arrayCookie = splitCookies();
-    for(let a = 0; a<arrayCookie.length; a++){
+    for (let a = 0; a < arrayCookie.length; a++) {
 
         if (findEmail(array[0].value, arrayCookie[a])) {
 
@@ -211,24 +227,24 @@ function findCookie(array){
     return 0;
 }
 
-function splitCookies(){
+function splitCookies() {
     var splited = document.cookie.split(";");
     return splited;
 }
 
-function findEmail(email, cookie){
+function findEmail(email, cookie) {
     var cookieemail = cookie.split(",");
     var positionequal = cookieemail[0].indexOf('=');
     let emailCookie = cookieemail[0].substring(0, positionequal);
-    if(emailCookie == email){
+    if (emailCookie == email) {
         return true;
     }
     return false;
 }
 
-function findPass(password, cookie){
+function findPass(password, cookie) {
     var cookiepassword = cookie.split(",");
-    if(cookiepassword[1] == password){
+    if (cookiepassword[1] == password) {
         return true;
     }
     return false;
