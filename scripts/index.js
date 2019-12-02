7bbf617b59556b2311674a9f61523f938cd86e4b
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-12-02 14:38:56 +0100

    List

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,57 @@
 $(document).ready(function () {
 
+    addEventListeners();
+
+    $(".new-event").on('submit', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        let title = $(this).find('input[name="title"]').val();
+        let date = $(this).find('input[name="date"]').val();
+        $(this).parent().find('.event-list').append(`
+            <div class="event">
+                <a class="close" href="#"><img src="images/close.png" alt="close"></a>
+                <h3>`+ title +`</h3>
+                <ul class="event-description">
+                    <li><img class="like new" src="images/like.png" alt="like"></li>
+                    <li>`+ date +`</li>
+                </ul>
+            </div>
+        `)
+        $(this).find('input[name="title"]').val('')
+        $(this).find('input[name="date"]').val('')
+        addEventListeners();
+    });
+
+    $(".new-category").on('submit', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        let title = $(this).find('input[name="title"]').val();
+        $(this).before(`
+            <div class="category">
+                <a class="category-options" href="#"><img src="images/menu.png" alt="options"></a>
+                <div class="category-options-dropdown">
+                    <a class="close-category" href="#">Archive list</a>
+                    <a href="#">Clone list</a>
+                    <a href="#">Share list</a>
+                    <a href="#">Settings</a>
+                </div>
+                <h1>`+ title + `</h1>
+                <div class="event-list">
+                </div>
+                <form class="new-event">
+                    <input class="form-control mr-sm-1" name="title" placeholder="Title" aria-label="New Event">
+                    <input class="form-control mr-sm-1" name="date" type="date" aria-label="date">
+                </form>
+            </div>
+        `)
+        $(this).find('input[name="title"]').val('')
+        addEventListeners();
+    });
+
+});
+
+function addEventListeners(){
+
     // Close event
     $(".close").on('click', function () {
         if (confirm("Are you sure you want to delete this event?")) {
@@ -8,7 +60,7 @@ $(document).ready(function () {
     });
 
     // Like event
-    $(".like").on('click',function () {
+    $(".like").on('click', function () {
         if ($(this).attr('src') == 'images/like.png') {
             $(this).attr('src', 'images/like-active.png');
         } else {
@@ -28,18 +80,18 @@ $(document).ready(function () {
         }
     });
 
-    // Drop a event inside a column
+    // Drop a event inside a category
     $(".event-list").sortable({
         items: ".event",
     }).disableSelection();
 
-    // Drop a event inside a column
+    // Drop a event inside a category
     $(".event-list").droppable({
         accept: ".event",
     });
 
     // Drag a category
-    $('.column').draggable({
+    $('.category').draggable({
         cursor: 'move',
         delay: 200,
         connectToSortable: ".board",
@@ -49,52 +101,34 @@ $(document).ready(function () {
         }
     });
 
-    // Drop a column inside board
+    // Drop a category inside board
     $(".board").sortable({
-        items: ".column",
+        items: ".category",
     });
 
-    // Drop a column inside board
+    // Drop a category inside board
     $(".board").droppable({
-        accept: ".column",
+        accept: ".category",
     });
 
-    // Show column options 
-    $('.column-options').on('click',function (event) {
-        $('.column-options-dropdown').hide();
-        $(this).parent().find('.column-options-dropdown').css('display', 'flex');
+    // Show category options 
+    $('.category-options').on('click', function (event) {
+        $('.category-options-dropdown').hide();
+        $(this).parent().find('.category-options-dropdown').css('display', 'flex');
         event.stopPropagation();
     });
 
-    // Hide column options when click outside 
-    $(document).on('click',function () {
-        $('.column-options-dropdown').hide();
+    // Hide category options when click outside 
+    $(document).on('click', function () {
+        $('.category-options-dropdown').hide();
     });
 
-    // Hide column when click on close column
-    $('.close-column').on('click', function (event) {
-        if (confirm("Are you sure you want to delete this column?")) {
+    // Hide category when click on close category
+    $('.close-category').on('click', function (event) {
+        if (confirm("Are you sure you want to delete this category?")) {
             $(this).parent().parent().hide("3000");
         }
     });
 
-    $(".new-event").on('submit',function (event) {
-        let title = $(this).find('input[name="title"]').val();
-        $(this).before(`
-            <div class="event">
-                <a class="close" href="#"><img src="images/close.png" alt="close"></a>
-                <h3>`
-                +title+
-                `</h3>
-                <ul class="event-description">
-                    <li><img class="like new" src="images/like.png" alt="like"></li>
-                    <li>03/06/19 15:47</li>
-                </ul>
-            </div>
-        `)
-        $(this).find('input[name="title"]').val('') 
-        event.preventDefault();
-    });
-
-});
-
+}
+ 
